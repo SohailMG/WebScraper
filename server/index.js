f54f839d6121708917791ff8f2c07767b1ff3403
Sohail Gsais <gsais.sohail9@gmail.com>
2021-03-05 11:57:05 +0000

    added signup functionality

---
+++ server/index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const db = require("./mysql-connect");
+const requestAPI = require("request");
+const app = express();
+const port = 3000;
+const APIEndpoint = "http://api.tvmaze.com/";
+const scrapeTrending = require("./scrapper");
+const expressSession = require('express-session');
+
+app.use(express.static('../public'));
+app.use(bodyParser.json());
+
+let showInfo = [];
+
+//handles post request of show data being added
+app.post("/shows", (request, response) => {
+  //stores show data into database
+  let show = request.body.Show;
+  db.storeNewShow(
+    show.name,
+    show.Genres,
+    show.Rating,
+    show.image,
+    show.Premiered
+  );
+
+
+  response.send({ message: "Show added" });
+});
+app.post("/user", (request, response) => {
+  //stores show data into database
+  let user = request.body.userInfo;
+  db.storeNewUser(
+    user.username,
+    user.email,
+    user.password
+  )
+
+
+  response.send({ message: "user added" });
+});
+
+/**
+ * scrapes website titles of top 5 shows currently trending
+ * then makes get request for api on info for each title
+ */
+(async function getTrendingShows() {
+  db.deleteCurrentData();
+  const showsArr = await scrapeTrending();
+  showsArr.forEach((showTitle) => {
+    requestAPI(
+      `${APIEndpoint}/singlesearch/shows?q=${showTitle}`,
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          data = JSON.parse(body);
+          // console.log(JSON.parse(body));
+        }
+        // storing show details into database
+        db.storeTrendings(
+          data.id,
+          data.name,
+          data.genres,
+          data.rating.average,
+          data.image.medium,
+          data.premiered
+        );
+      }
+    );
+  });
+})();
+
+app.get("/shows", (request, response) => {
+  //Output the data sent to the server
+  let allData = db.getEmployees();
+  allData
+    .then((result) => {
+      //Do something else
+      let data = result;
+      showInfo.push(data.slice(0, 6));
+      // console.log(data);
+      // console.log(showInfo[0]);
+      response.send(showInfo[1]);
+    })
+    .catch((err) => {
+      //Handle the error
+      console.error(JSON.stringify(err));
+    });
+});
+
+
+app.post("/search", getSearches);
+
+/**
+ * performs a get request to an API with the search
+ * queries and returns all matching results with a maximum
+ * of 5 searches
+ * @param {JSON} req 
+ * @param {JSON} res 
+ */
+function getSearches(req, res) {
+  const MAX_SEARCHES = 5;
+  let searchQuery = req.body.Title;
+  let showD = [];
+  // get request to api with search query
+  requestAPI(
+    `${APIEndpoint}/search/shows?q=${searchQuery}`,
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        data = JSON.parse(body);
+        for (let i = 0; i < MAX_SEARCHES; i++) {
+          if (data[i] != undefined) {
+            if (data[i].show.image != null) {
+              let shows = {
+                name: data[i].show.name,
+                genres: data[i].show.genres,
+                ratings: data[i].show.rating.average,
+                premiered: data[i].show.premiered,
+                image: data[i].show.image.medium,
+              };
+              showD.push(shows);
+            } else {
+            }
+          }
+        }
+        res.send(showD);
+      }
+    }
+  );
+}
+
+app.listen(port);
---
+++ server/mysql-connect.js
@@ -0,0 +1,90 @@
+const request = require("express");
+const xhttp = require("http");
+const axios = require("axios");
+
+const mysql = require("mysql");
+
+let connectionPool = mysql.createPool({
+  connectionLimit: 1,
+  host: "localhost",
+  user: "root",
+  password: "rootpass123",
+  database: "scrapperApp",
+  debug: false,
+});
+
+//Build query
+
+function storeNewShow( name, genres, ratings, image, premiered) {
+  let sql = `INSERT INTO TVShows (id, name, genres, ratings, premiered,image)        
+  VALUES (default, '${name}', '${genres}', '${ratings}', '${premiered}', '${image}')`;
+
+  connectionPool.query(sql, (err, result) => {
+    if (err) {
+      //Check for errors
+      console.error(`Error executing query: ${JSON.stringify(err)}`);
+    } else {
+      console.log(JSON.stringify(result));
+    }
+  });
+}
+
+function storeTrendings(id, name, genres, ratings, image, premiered) {
+  let sql = `INSERT INTO Trending (id, name, genres, ratings, premiered,image)        
+  VALUES (default, '${name}', '${genres}', '${ratings}', '${premiered}', '${image}')`;
+
+  connectionPool.query(sql, (err, result) => {
+    if (err) {
+      //Check for errors
+      console.error(`Error executing query: ${JSON.stringify(err)}`);
+    } else {
+      console.log(JSON.stringify(result));
+    }
+  });
+}
+
+function deleteCurrentData(){
+    //Delete all customers with the address "Mountain 21":
+    var sql = "DELETE FROM Trending"
+    connectionPool.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+}
+/* Outputs all of the employees */
+
+/* Returns a promise to get employees. */
+async function getEmployees() {
+  //Build query
+  let sql = "SELECT * FROM Trending";
+  //Wrap the execution of the query in a promise
+  return new Promise((resolve, reject) => {
+    connectionPool.query(sql, (err, result) => {
+      if (err) {
+        //Check for errors
+        reject("Error executing query: " + JSON.stringify(err));
+      } else {
+        //Resolve promise with results
+        resolve(result);
+      }
+    });
+  })
+}
+
+function storeNewUser( name, email, password) {
+  let sql = `INSERT INTO Users (user_id, name, email, password)        
+  VALUES (default, '${name}', '${email}', '${password}')`;
+
+  connectionPool.query(sql, (err, result) => {
+    if (err) {
+      //Check for errors
+      console.error(`Error executing query: ${JSON.stringify(err)}`);
+    } else {
+      console.log(JSON.stringify(result));
+    }
+  });
+}
+
+module.exports = { storeNewUser,storeNewShow, getEmployees ,storeTrendings,deleteCurrentData};
+
+//Execute query and output resul
